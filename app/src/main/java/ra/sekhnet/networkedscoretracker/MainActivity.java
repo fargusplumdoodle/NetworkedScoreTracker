bed28ee867d7e7445009942192e4789954012979
fargus plumdoodle <dont@worry.about.it>
2019-03-08 14:48:20 -0800

    added default names, players now are given random names to start with

--- app/src/main/java/ra/sekhnet/networkedscoretracker/MainActivity.java
+++ app/src/main/java/ra/sekhnet/networkedscoretracker/MainActivity.java
@@ -2,6 +2,9 @@ package ra.sekhnet.networkedscoretracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // generating example name
+        Random rand = new Random();
+        String[] exampleNames = new String[]{"Robyn Kircher",
+                "YolandoBerndt",
+                "JuniorSettles",
+                "DorindaCoombes",
+                "MickiOrso",
+                "AntonettaHocking",
+                "YerSitler",
+                "CorrinBilodeau",
+                "ShellieNolton",
+                "BrettHankin",
+                "LashayMounts",
+                "ConsuelaSlaybaugh",
+                "YenStille",
+                "DanicaShephard",
+                "YukiVanmatre",
+                "JosefLaberge",
+                "TovaMccardell",
+                "TandraHanselman",
+                "GillianFujimoto",
+                "SpringBailon"};
+
+        String defaultPlayerName = exampleNames[rand.nextInt(exampleNames.length - 1)] + rand.nextInt(100);
+        TextView playerName = findViewById(R.id.PlayerNameEditText);
+        playerName.setText(defaultPlayerName);
     }
 }
