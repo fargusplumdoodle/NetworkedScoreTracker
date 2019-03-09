b421d891097697c06a771c4eb56c8d8e61950484
Fargus <fargusplumdoodle@gmail.com>
2019-03-09 13:09:41 -0800

    New game screen now shows list of joined players, start game button does nothing.
    Layout is terrible but works

--- app/src/main/java/ra/sekhnet/networkedscoretracker/NewGameActivity.java
+++ app/src/main/java/ra/sekhnet/networkedscoretracker/NewGameActivity.java
@@ -1,8 +1,10 @@
 package ra.sekhnet.networkedscoretracker;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class NewGameActivity extends AppCompatActivity {
@@ -19,5 +21,36 @@ public class NewGameActivity extends AppCompatActivity {
         TextView playerNameTextView = (TextView) findViewById(R.id.PlayerNameViewServerNewGame);
         playerNameTextView.setText(playerName);
 
+        // populating initial player list
+        this.getPlayerList();
+
+    }
+
+    private void getPlayerList(){
+        // this just gets each player who has joined the game and puts it in a list
+        String[] players = {"Dylan", "Liyani", "Cody"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                players);
+        ListView listView = (ListView) findViewById(R.id.joinedPlayerListView);
+        listView.setAdapter(adapter);
+        /*
+        List<HashMap<String, String>> aList = new ArrayList<HashMap<String, String>>();
+
+        for (int i = 0; i < players.length; i++){
+            // populating list, you could add the players health
+            HashMap<String, String> hm = new HashMap<String, String>();
+            hm.put("PlayerName", players[i]);
+            aList.add(hm);
+        }
+
+        String[] from = {"PlayerName"};
+        int[] to = {R.id.joinedPlayerListView};
+
+        SimpleAdapter theAdapter = new SimpleAdapter(getBaseContext(), aList, R.layout.activity_new_game, from, to);
+        ListView theListView = (ListView) findViewById(R.id.joinedPlayerListView);
+        theListView.setAdapter(theAdapter);
+        */
     }
 }
