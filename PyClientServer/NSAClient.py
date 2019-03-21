c7f43e7a544263f1af1b4316055d76cb6523a10a
fargus plumdoodle <dont@worry.about.it>
2019-03-21 15:12:55 -0700

    Start_game protocol ready to be implemented

--- PyClientServer/NSAClient.py
+++ PyClientServer/NSAClient.py
@@ -3,7 +3,7 @@ import socket
 
 class NSAClient(object):
     def __init__(self, name):
-        self.port = 8989
+        self.port = 7989
         self.packet_len = 1024
         self.server_addr = ('localhost', self.port)
         self.c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -22,6 +22,8 @@ class NSAClient(object):
             5. server says OK
 
         The server adds the clients connection to list of connections
+
+        The client then waits for the game to start
         """
         # 2
         self.c.connect(self.server_addr)
@@ -38,6 +40,14 @@ class NSAClient(object):
         print('Join game protocol complete')
 
     def start_game(self):
+        """
+        Protocol:
+            For each client:
+                1. client listens for the START_GAME signal
+                2. client says OK
+            3. Once each client has responded with OK
+            4. Server sends initial life total
+        """
         pass
 
     def life_update(self):
@@ -60,4 +70,8 @@ class NSAClient(object):
         self.c.send(msg)
 
 if __name__ == '__main__':
-    c = NSAClient('Fargus')
+    import sys
+    try:
+        c = NSAClient(sys.argv[1])
+    finally:
+        c.c.close()
--- PyClientServer/NSAServer.py
+++ PyClientServer/NSAServer.py
@@ -1,3 +1,5 @@
+import select
+import sys
 from collections import deque
 import socket
 import time
@@ -7,16 +9,16 @@ import threading
 class STATES:
     HOST_GAME = 0
     IN_GAME = 1
+    STARTING_GAME = 2
 
 
 class SockManager(threading.Thread):
     def __init__(self):
-        print('SockManager works')
         super(SockManager, self).__init__()
         self.q = deque()
         self.active_conns = set()
         self.num_clients_served = 0
-        self.port = 8989
+        self.port = 7989
         self.max_q_size = 3
 
         self.s = self.get_tcp_socket()
@@ -45,25 +47,51 @@ class SockManager(threading.Thread):
             # First check if any active connections
             if (len(self.q)) == 0:
                 # NO active connections, wait a sec and try again
-                time.sleep(2)
+                print("Queue empty, you have 3 seconds to start the game...")
+                i, o, e = select.select([sys.stdin], [], [], 3)
+                if i:
+                    print("user has instructed to start the game")
+                    self.state = STATES.STARTING_GAME
+
+                    # this is to stop listening for new clients
+                    c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    c.connect(('localhost', self.port))
+                    c.send("DONE".encode('utf-8'))
+
+                    # this works because this loop will stop running once the states have changed
                 continue
             else:
                 # initiating start_game protocol with client
-                print('Starting handling client')
                 client_info = self.q.pop()
                 ch = ClientHandler(client_info[0], client_info[1])
                 ch.host_game()
                 self.players[ch.client_name] = ch
 
+    def start_game(self):
+        """
+        Protocol:
+            For each client:
+                1. client listens for the START_GAME signal
+                2. client says OK
+            3. Once each client has responded with OK
+            4. Server sends initial life total
+        """
+        pass
 
     def run(self):
         print("Starting server on port %s" % self.port)
-        if self.state == STATES.HOST_GAME:
-            while True:
+        while True:
+            if self.state == STATES.HOST_GAME:
                 # getting new client and adding to  queue
                 client_soc, client_addr = self.s.accept()
                 self.q.append((client_soc, client_addr))
-                print('Got client')
+                print('Got client: ', client_addr)
+
+            elif self.state == STATES.STARTING_GAME:
+                print('STARTING GAME NOW')
+                print('Players:', self.players)
+
+                break
 
 
 class ClientHandler(threading.Thread):
@@ -76,7 +104,7 @@ class ClientHandler(threading.Thread):
         self.client_name = ''
         self.packet_len = 1024
 
-    def run(self, state):
+    def run(self):
         pass
 
     def host_game(self):
@@ -89,6 +117,8 @@ class ClientHandler(threading.Thread):
             5. server says OK
 
         The server adds the clients connection to list of connections
+
+        The client then waits for the game to start
         """
         # 3. server says READY
         print('Sending ready')
@@ -97,13 +127,23 @@ class ClientHandler(threading.Thread):
         # 4. clients send the name of the player ( e.g: "Fargus" )
         # TODO: Error handling
         self.client_name = self.c.recv(self.packet_len).decode('utf-8')
+
         print('Client name ', self.client_name)
 
         # 5. server says OK
         self.c.send('OK'.encode('utf-8'))
-        pass
+
+        # When the user running the server starts the game, we will move to start game
 
     def start_game(self):
+        """
+        Protocol:
+            For each client:
+                1. client listens for the START_GAME signal
+                2. client says OK
+            3. Once each client has responded with OK
+            4. Server sends initial life total
+        """
         pass
 
     def life_update(self):
@@ -112,7 +152,18 @@ class ClientHandler(threading.Thread):
     def recieve_life(self):
         pass
 
+    def recv_expect(self, expected_msg):
+        response = self.c.recv(self.packet_len).decode('utf-8')
+        if response != expected_msg:
+            raise ValueError('Error: Invalid response from client, expecting %s got %s' % (expected_msg, response))
+
+
+
 if __name__ == '__main__':
     man = SockManager()
-    man.start()
-    man.pre_game_handle_clients()
+    try:
+        man.start()
+        man.pre_game_handle_clients()
+    finally:
+        man.s.close()
+
